15150655d6a3ec3618529966c025a66197996350
Tandian <tandianbinta2004@email.com>
2024-12-11 16:49:56 +0100

    modification du css et ajout des views de Cam

--- Views/view_connexion.php
+++ Views/view_connexion.php
@@ -1,21 +1,47 @@
-<?php $title= "Connexion";
+<?php
+$title = "Connexion";
+$bodyClass = "background-modifie"; // Définit la classe pour le body
 require "view_begin.php";
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="" method="post">
-        <p>Email :<input type="text" name="email"></p>
-        <p><input type="submit" value="Connexion"></p>
-    </form>
-    <p><a href="?controller=Connexion&action=register">Register</a></p>
-</body>
-</html>
+<!--container-fluid permet d'obtenir un affichage responsive-->
+<div id="cont_co" class="container-fluid">
+    <div class="row justify-content-center align-items-center">
+        <div class="col-lg-6 col-md-8 col-sm-10 col-12 form_co">
+            <p class="co"><strong>Connexion</strong></p>
+
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="id">
+                        <img class='icone_co' src='Content/img/mail.png' />
+                        Email
+                    </label>
+                    <input class="form-control" type="text" name="email" id="email" required />
+                </div>
+
+                <div class="form-group">
+                    <label for="mdp">
+                        <img class='icone_co' src='Content/img/mdp.png' />
+                        Mot de passe
+                    </label>
+                    <input class="form-control" type="password" name="mdp" id="mdp" required />
+                </div>
+
+                <?php if (isset($message)): ?>
+                    <p class='psswrd'> <?= $message ?> </p>
+                <?php endif ?>
+                <p>
+                    <a href="?controller=Connexion&action=register">S'inscrire</a>
+                </p>
+                <p>
+                    <a href="forgetmdp.php">Mot de passe oublié?</a>
+                </p>
+                <label>
+                    <input class="bouton" type="submit" value="Se connecter" />
+                </label>
+            </form>
+        </div>
+    </div>
+</div>
 
 <?php require "view_end.php"; ?>
