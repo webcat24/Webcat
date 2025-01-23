6932da490193b9812658b904ed1ec65b4077b535
Galindo Adrian <galindo.adrian308@gmail.com>
2025-01-23 11:51:03 +0100

    images des peintures

--- Models/credentials.php
+++ Models/credentials.php
@@ -1,18 +1,14 @@
 <?php
 
-$dsn= "pgsql:host=localhost;dbname=sae_s5";
-$login= "postgres";
-$mdp = "root";
+// $dsn= "pgsql:host=localhost;dbname=sae_s5";
+// $login= "postgres";
+// $mdp = "root";
 
-$dsn= "pgsql:host=localhost;dbname=BDD_SAE";
-$login= "postgres";
-$mdp = "L@lexxx12!";
+// $dsn= "pgsql:host=localhost;dbname=saeS5";
+// $login= "postgres";
+// $mdp = "L@lexxx12!";
 
-// $dsn = "pgsql:host=localhost;port=5433;dbname=saes5";
-// $login = "postgres";
-// $mdp = "02062004";
-<<<<<<< HEAD
-=======
-
->>>>>>> 46ff660db0ebc9aac46b854a4232a79268687d68
+$dsn = "pgsql:host=localhost;port=5433;dbname=saes5";
+$login = "postgres";
+$mdp = "02062004";
 ?>
